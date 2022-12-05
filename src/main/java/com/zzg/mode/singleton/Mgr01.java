0e3bdaf489007c597794f34418cd4921e5ab0369
zzg <2521250151@qq.com>
2022-12-05 18:04:15 +0800

    单例模式

--- src/main/java/com/zzg/mode/singleton/Mgr01.java
+++ src/main/java/com/zzg/mode/singleton/Mgr01.java
@@ -1,2 +1,25 @@
-package com.zzg.mode.singleton;public class Mgr01 {
+package com.zzg.mode.singleton;
+
+/**
+ * 饿汉式
+ * 类加载器加载时就会实例化一个单例，jvm保证线程安全
+ * 优点：简单实用，推荐使用
+ * 缺点：无论是否使用都会有一个实例
+ * 单例模式
+ *
+ * @author 张中贵
+ */
+public class Mgr01 {
+    public static final Mgr01 INSTANCE = new Mgr01();
+
+    private Mgr01() {
+    }
+
+    public static Mgr01 getInstance() {
+        return INSTANCE;
+    }
+
+    public void test() {
+        System.out.println("这是一个饿汉形式的单例模式");
+    }
 }
--- src/test/java/com/zzg/mode/ModeApplicationTests.java
+++ src/test/java/com/zzg/mode/ModeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ModeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
